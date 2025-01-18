44fad3c4b2d2b2d0e86283df7d9a4fe61f82cc08
nuryll <52735279+nuryll@users.noreply.github.com>
2025-01-18 15:20:14 +0100

    Update TeacherForm.tsx

--- dashboard/src/components/forms/TeacherForm.tsx
+++ dashboard/src/components/forms/TeacherForm.tsx
@@ -22,7 +22,7 @@ const schema = z.object({
   bloodType: z.string().min(1, { message: "Blood Type is required!" }),
   birthday: z.date({ message: "Birthday is required!" }),
   sex: z.enum(["male", "female"], { message: "Sex is required!" }),
-  img: z.instanceof(File, { message: "Image is required" }),
+  img: z.instanceof(File, { message: "Image is required." }),
 });
 
 type Inputs = z.infer<typeof schema>;
